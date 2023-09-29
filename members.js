825ae0edcf240ac136b3e7381a5bb243c3152ae4
Rifki Baom <145200077+rifkibaom@users.noreply.github.com>
2023-09-29 07:52:24 +0000

    Copilot second commit

--- members.js
+++ members.js
@@ -1,15 +1,8 @@
-function skillMember()
-{
-    var member = document.getElementById("member").value;
-    var skill = document.getElementById("skill").value;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function()
-    {
-        if(this.readyState == 4 && this.status == 200)
-        {
-            document.getElementById("skillMember").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "skillMember.php?member="+member+"&skill="+skill, true);
-    xhttp.send();
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/members/skills.html',
+    controller: 'MembersCtrl',
+    controllerAs: 'membersCtrl'
+  };
 }
